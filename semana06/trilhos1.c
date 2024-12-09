0496c30230dc16c8955a20229616b36b0c3aa563
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2024-12-09 20:24:59 -0300

    Fiz os exercícios da semana 6

---
+++ semana06/trilhos1.c
@@ -0,0 +1,124 @@
+// Exercício 1062 do beecrowd
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct No {
+    int valor;
+    struct No *proximo;
+}no;
+
+
+typedef struct Pilha {
+    int tam;
+    no *topo;
+}pilha;
+
+
+void inicializaPilha(pilha *p) {
+    p->tam = 0;
+    p->topo = NULL;
+}
+
+
+void push(pilha *p, int valor) {
+    no *novo = (no*) malloc(sizeof(no));
+
+    // Se a memória foi alocada corretamente, a operação push é realizada:
+    if(novo) {
+        p->tam += 1;
+        novo->valor = valor;
+        novo->proximo = p->topo;
+        p->topo = novo;
+    }
+}
+
+
+void pop(pilha *p) {
+    // Verificando se a pilha não está vazia:
+    if(p->topo != NULL) {
+        p->tam -= 1;
+        no *remover = p->topo;
+        p->topo = remover->proximo;
+        free(remover);
+    }
+}
+
+
+void imprimirPilha(pilha p) {
+    while(p.topo) {
+        printf("%d\n", p.topo->valor);
+        p.topo = p.topo->proximo;
+    }
+}
+
+
+void deletarPilha(pilha *p) {
+    while(p->topo) {
+        pop(p);
+    }
+}
+
+int main() {
+    int n, x;
+    pilha a, b, resultado;
+
+    while(scanf("%d", &n) != EOF) {
+        if(!n) {
+            break;
+        }
+
+        while(scanf("%d", &x)) {
+            if(!x) {
+                printf("\n");
+                break;
+            }
+
+            inicializaPilha(&a);
+            inicializaPilha(&b);
+            inicializaPilha(&resultado);
+
+            push(&a, x);
+            push(&b, 1);
+
+            for(int i = 2; i <= n; i++) {
+                scanf("%d", &x);
+                push(&a, x);
+                push(&b, i);
+            }
+
+            while(a.tam != 0 || b.tam != 0 || resultado.tam != 0) {
+                if(a.tam != 0 && b.tam != 0 && a.topo->valor == b.topo->valor){
+                    pop(&a);
+                    pop(&b);
+                
+                }else if(resultado.tam != 0 && b.tam != 0 && resultado.topo->valor == b.topo->valor){
+                    pop(&resultado);
+                    pop(&b);
+                
+                }else if(a.tam != 0){
+                    push(&resultado, a.topo->valor);
+                    pop(&a);                
+                }
+                
+                else{
+                    break;
+                }
+            } 
+
+            if(a.tam == 0 && resultado.tam == 0 && b.tam == 0){
+                printf("Yes\n");
+            }
+            
+            else{
+                printf("No\n");
+            }
+
+            deletarPilha(&a);
+            deletarPilha(&b);
+            deletarPilha(&resultado);
+        }
+    }
+ 
+    return 0;
+}
---
+++ semana06/trilhos2.c
@@ -0,0 +1,113 @@
+// Exercício 1063 do beecrowd
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct No {
+    char letra;
+    struct No *proximo;
+}no;
+
+
+typedef struct Pilha {
+    int tam;
+    no *topo;
+}pilha;
+
+
+void inicializaPilha(pilha *p) {
+    p->tam = 0;
+    p->topo = NULL;
+}
+
+
+void push(pilha *p, char letra) {
+    no *novo = (no*) malloc(sizeof(no));
+
+    // Se a memória foi alocada corretamente, a operação push é realizada:
+    if(novo) {
+        p->tam += 1;
+        novo->letra = letra;
+        novo->proximo = p->topo;
+        p->topo = novo;
+    }
+}
+
+
+void pop(pilha *p) {
+    // Verificando se a pilha não está vazia:
+    if(p->topo != NULL) {
+        p->tam -= 1;
+        no *remover = p->topo;
+        p->topo = remover->proximo;
+        free(remover);
+    }
+}
+
+
+void imprimirPilha(pilha p) {
+    while(p.topo) {
+        printf("%d\n", p.topo->letra);
+        p.topo = p.topo->proximo;
+    }
+}
+
+
+void deletarPilha(pilha *p) {
+    while(p->topo) {
+        pop(p);
+    }
+}
+
+int main() {
+    int n;
+    char x;
+    pilha p;
+    char a[30], b[30];
+
+    while(scanf("%d", &n) && n) {
+        inicializaPilha(&p);
+        
+        for(int i = 0; i < n; i++) {
+            scanf(" %c", &a[i]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            scanf(" %c", &b[i]);
+        }
+        
+        a[n] = '\0';
+        b[n] = '\0';
+
+        int i = 0, j = 0;
+        while(1) {
+            if(p.tam != 0 && j < n && p.topo->letra == b[j]){
+                pop(&p);
+                printf("R");
+                j++;
+            }
+                
+            else if(i < n){
+                push(&p, a[i]);
+                printf("I");
+                i++;
+            }
+
+            else{
+                break;
+            }
+        } 
+    
+        if(p.tam == 0) {
+            printf("\n");
+        }
+            
+        else {
+            printf(" Impossible\n");
+        }
+
+        deletarPilha(&p);   
+    }
+ 
+    return 0;
+}
